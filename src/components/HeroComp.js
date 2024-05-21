ba47f476dd338dc8eb2922a2b37a868ebab917aa
alfajarjaya <sansanjaya40@gmail.com>
2024-05-21 14:31:03 +0700

    new animation

--- src/components/HeroComp.js
+++ src/components/HeroComp.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import AOS from "aos";
+import '../../node_modules/aos/dist/aos.css';
 
 const HeroComp = () => {
     const scrollToSection = (id) => {
@@ -9,27 +11,37 @@ const HeroComp = () => {
         }
     };
 
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+    }, []);
+
     return (
         <div className="hero min-vh-100 w-100 d-flex align-items-center" id="home">
             <Container>
                 <Row className="text-center">
                     <Col>
-                        <h1 className="display-3 text-white mb-4">Welcome to My Portfolio</h1>
-                        <p className="lead text-white-75 mb-4 text-warning">
-                            Discover my work, projects, and get in touch for collaborations.
+                        <h1 className="display-3 text-white text-bold mb-4" data-aos="fade-left">
+                            <u>Hi, I'm Fajar</u></h1>
+                        <p className="lead text-white-75 mb-4 text-warning" data-aos="fade-right">
+                            Just a Student's.
                         </p>
-                        <Button 
-                            variant="primary" 
-                            size="lg" 
-                            className="me-2" 
+                        <Button
+                            variant="primary"
+                            size="lg"
+                            className="me-2"
                             onClick={() => scrollToSection('about')}
+                            data-aos="fade-up"
+                            data-aos-delay="100"
                         >
                             Learn More
                         </Button>
-                        <Button 
-                            variant="outline-light" 
-                            size="lg" 
+                        <Button
+                            variant="outline-light"
+                            size="lg"
                             onClick={() => scrollToSection('contact')}
+                            data-aos="fade-up"
+                            data-aos-delay="200"
                         >
                             Contact Me
                         </Button>
--- src/components/Service.js
+++ src/components/Service.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import AOS from "aos";
+import '../../node_modules/aos/dist/aos.css';
 
 const importAll = (r) => r.keys().map(r);
 let images = importAll(require.context('../assets/img/skills/', false, /\.(png|jpe?g|svg)$/));
 
 const About = () => {
+    const [showAll, setShowAll] = useState(false);
 
-    const showAllProject = () => {
-        window.open('https://bangjarrr.github.io/my-project/', '_blank');
+    const toggleShowAll = () => {
+        setShowAll(!showAll);
     }
+
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+    }, []);
 
     return (
         <div className="d-flex flex-column min-vh-100 justify-content-center w-body text-white" id="about">
             <Container>
                 <Row>
                     <Col className="text-center mt-5">
-                        <h1>About Me</h1>
+                        <h1 data-aos="fade-down">About Me</h1>
                     </Col>
                 </Row>
                 <Row className="justify-content-center mt-5">
-                    <Col md={8} className="text-start">
+                    <Col md={8} className="text-start" data-aos="fade-down">
                         <p>
                             Hi, I'm Mochammad Alfajjar, usually called Fajar. I'm from East
                             Java, Indonesia 🇮🇩. I am a vocational high school student,
@@ -34,11 +42,11 @@ const About = () => {
                 </Row>
                 <Row>
                     <Col className="text-center mt-5">
-                        <h1>About Fajar Pages</h1>
+                        <h1 data-aos="fade-down">About Fajar Pages</h1>
                     </Col>
                 </Row>
                 <Row className="justify-content-center">
-                    <Col md={8} className="text-start mt-5">
+                    <Col md={8} className="text-start mt-5" data-aos="fade-down">
                         <p>
                             <a href="https://bangjarrr.github.io" className="text-warning" target="_blank">Fajar Pages</a> is a website created in December 2023. I made this website as a personal record of what I have done, be it my project or other things.
                             Hopefully this website can be useful for all of you; Hopefully we can work together to advance this beloved country through programming or other things related to technology.
@@ -48,7 +56,7 @@ const About = () => {
                 </Row>
                 <Row className="justify-content-center mt-5">
                     <Col className="text-center">
-                        <h1>Tech Stack</h1>
+                        <h1 data-aos="fade-up">Tech Stack</h1>
                         <hr />
                     </Col>
                 </Row>
@@ -59,6 +67,8 @@ const About = () => {
                                 src={image}
                                 alt={`Skill ${index + 1}`}
                                 className="img-fluid skills-img p-3"
+                                data-aos="fade-up"
+                                data-aos-delay={index * 100}
                             />
                         </Col>
                     ))}
@@ -71,16 +81,30 @@ const About = () => {
                 </Row>
                 <Row className="justify-content-center mb-5">
                     <Col md={8} className="mt-5 img-fluid">
-                        <div className="mb-3 project">
-                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/QofPahiYfMo?si=RSePFEM7W1Pg5VsT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
-                            <p>Website Coffe Shop.</p>
+                        <div>
+                            <div className="mb-3 project">
+                                <iframe width="100%" height="315" src="https://www.youtube.com/embed/QofPahiYfMo?si=RSePFEM7W1Pg5VsT" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                                <p>Website Coffee Shop.</p>
+                            </div>
+                            <div className="mb-3 project">
+                                <iframe width="100%" height="315" src="https://www.youtube.com/embed/os4mnM_xepo?si=TQ-L5KLuXko4aebw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                <p>Manajemen Perpuskataan with Python.</p>
+                            </div>
+
+                            {showAll && (
+                                <div>
+                                    <div className="mb-3 project">
+                                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/ihBd1PjKnLY?si=1yMyr9D-7YzzQvV4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                                        <p>Translate Python GUI.</p>
+                                    </div>
+                                </div>
+                            )}
                         </div>
-                        <div className="mb-3 project">
-                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/ihBd1PjKnLY?si=1yMyr9D-7YzzQvV4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
-                            <p>Translate Pytho GUI.</p>
-                        </div>
+
                         <div className="justify-content-center text-center mt-5 mb-5">
-                            <Button variant="outline-warning" onClick={showAllProject}>Show All Project</Button>{' '}
+                            <Button variant="outline-warning" onClick={toggleShowAll}>
+                                {showAll ? "Close" : "Show All Project"}
+                            </Button>{' '}
                         </div>
                     </Col>
                 </Row>
