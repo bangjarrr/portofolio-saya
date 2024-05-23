f6f7b09befc5e6a13539d044bc9f3fcf5cf9956b
bangjarrr <alfajjar16@gmail.com>
2024-05-24 05:38:46 +0700

    new p

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import AOS from "aos";
 import '../../node_modules/aos/dist/aos.css';
+import '../css/main.css';
 
 const importAll = (r) => r.keys().map(r);
 let images = importAll(require.context('../assets/img/skills/', false, /\.(png|jpe?g|svg)$/));
+let skills = ["HTML", "CSS", "JavaScript", "Python", "My SQL"];
 
 const About = () => {
     const [showAll, setShowAll] = useState(false);
@@ -62,14 +64,17 @@ const About = () => {
                 </Row>
                 <Row className="justify-content-center mb-5">
                     {images.map((image, index) => (
-                        <Col key={index} md={4} className="text-center mt-5">
-                            <img
-                                src={image}
-                                alt={`Skill ${index + 1}`}
-                                className="img-fluid skills-img p-3"
-                                data-aos="fade-up"
-                                data-aos-delay={index * 100}
-                            />
+                        <Col key={index} md={4} className="text-center mt-5 d-flex justify-content-center">
+                            <div className="skill-img-container" data-aos="fade-up" data-aos-delay={index * 100}>
+                                <img
+                                    src={image}
+                                    alt={skills[index]}
+                                    className="img-fluid skills-img py-3"
+                                />
+                                <div className="overlay">
+                                    <div className="text text-warning">{skills[index]}</div>
+                                </div>
+                            </div>
                         </Col>
                     ))}
                 </Row>
@@ -84,18 +89,18 @@ const About = () => {
                         <div>
                             <div className="mb-3 project">
                                 <iframe width="100%" height="315" src="https://www.youtube.com/embed/QofPahiYfMo?si=RSePFEM7W1Pg5VsT" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                                <p>Website Coffee Shop.</p>
+                                <p>Website Coffee Shop. Show demo <a href="https://kedai-kopi-kita.netlify.app/halaman-utama/" target="_blank" rel="noreferrer" className="text-warning">in here</a>.</p>
                             </div>
                             <div className="mb-3 project">
                                 <iframe width="100%" height="315" src="https://www.youtube.com/embed/os4mnM_xepo?si=TQ-L5KLuXko4aebw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                                <p>Manajemen Perpuskataan with Python.</p>
+                                <p>Manajemen Perpuskataan with Python. Show demo <a href="https://manajemen-perpustakaan.vercel.app/" target="_blank" rel="noreferrer" className="text-warning">in here</a>.</p>
                             </div>
 
                             {showAll && (
                                 <div>
                                     <div className="mb-3 project">
                                         <iframe width="100%" height="315" src="https://www.youtube.com/embed/ihBd1PjKnLY?si=1yMyr9D-7YzzQvV4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                                        <p>Translate Python GUI.</p>
+                                        <p>Translate Python GUI. Show demo <a href="https://translate-python.vercel.app/" target="_blank" rel="noreferrer" className="text-warning">in here</a>.</p>
                                     </div>
                                 </div>
                             )}
