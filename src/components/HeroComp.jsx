f0222ce0693c265bfca81fc26647a943a15265ab
alfajarjaya <sansanjaya40@gmail.com>
2024-05-31 19:35:43 +0700

    rawr

--- src/components/HeroComp.jsx
+++ src/components/HeroComp.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import AOS from "aos";
 import '../../node_modules/aos/dist/aos.css';
@@ -16,6 +16,26 @@ const HeroComp = () => {
         AOS.refresh();
     }, []);
 
+    const title = "Just a Student's. ";
+    const [heading, setHeading] = useState("");
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setHeading(prevHeading => {
+                if (count === title.length) {
+                    setCount(0);
+                    return "";
+                } else {
+                    setCount(count + 1);
+                    return prevHeading + title[count];
+                }
+            });
+        }, 200);
+
+        return () => clearInterval(interval);
+    }, [count, title]);
+
     return (
         <div className="hero min-vh-100 w-100 d-flex align-items-center" id="home">
             <Container>
@@ -23,8 +43,8 @@ const HeroComp = () => {
                     <Col>
                         <h1 className="display-3 text-white text-bold mb-4" data-aos="fade-left">
                             <u>Hi, I'm Fajar</u></h1>
-                        <h2 className="lead text-white-75 mb-4 text-warning" data-aos="fade-right">
-                            Just a Student's.
+                        <h2 className="lead text-white-75 mb-4 text-warning " data-aos="fade-right" id="heading">
+                            {heading}
                         </h2>
                         <Button
                             variant="warning"
